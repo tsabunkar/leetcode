659e47bb54c3531a97e0ce352b8e7361ca0d59b6
Sabunkar Tejas Shailesh <tsabunkar@gmail.com>
2025-06-08 19:09:45 +0530

    singly linked list

--- 876.js
+++ 876.js
@@ -1,5 +1,4 @@
 // 876. Middle of the Linked List
-import { Math } from "Math";
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -12,6 +11,7 @@ import { Math } from "Math";
  * @param {ListNode} head
  * @return {ListNode}
  */
+/* 
 var middleNode = function (head) {
   let headLength = head.length;
   let middle;
@@ -23,6 +23,57 @@ var middleNode = function (head) {
   const wholeMiddle = Math.ceil(middle * 1) * 1.0;
   return [head[wholeMiddle - 1], head[wholeMiddle], head[wholeMiddle + 1]];
 };
+ */
 
-console.log(middleNode([1, 2, 3, 4, 5]));
-console.log(middleNode([1, 2, 3, 4, 5, 6]));
+// Considering its not array but linked list, so cannot use any of the Array inbuilt methods
+
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+function buildLinkedList(arr) {
+  let head = new ListNode(0);
+  let current = head;
+  for (let val of arr) {
+    current.next = new ListNode(val);
+    current = current.next;
+  }
+  return head.next; // head of the actual list
+}
+
+const linkedList1 = buildLinkedList([1, 2, 3, 4, 5, 6]);
+// console.log(linkedList1);
+
+var middleNode = function (head) {
+  let headlength = 0;
+  let current = head;
+
+  // find out the length of the linked list
+  while (current) {
+    console.log(current.val);
+    headlength = headlength + 1;
+    current = current.next;
+  }
+  console.log("headlength is ", headlength);
+
+  // find out the middle node of the linked list
+  let middle;
+  if (headlength % 2 === 0) {
+    middle = headlength / 2 + 1;
+  } else {
+    middle = headlength / 2;
+  }
+  console.log(middle);
+  const wholeMiddle = Math.ceil(middle * 1) * 1.0;
+  console.log("wholeMiddle length is ", wholeMiddle);
+
+  // return the singlylinkedlist from the middlenode
+
+  let singlylinkedlistFromMiddlenode = new ListNode(0);
+};
+
+console.log(middleNode(linkedList1));
+// console.log(middleNode([1, 2, 3, 4, 5, 6]));
